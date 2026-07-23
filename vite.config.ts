df57661a81839f215d257417295557a135e194c7
kahlaoui <kahlaoui.ghaieth@gmail.com>
2026-07-23 23:25:01 +0200

    feat: add Vite plugins for Vuetify and Vue components, and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      globs: ['src/**/*.vue'],
+    }),
+    vuetify({ autoImport: true }),
     vueDevTools(),
   ],
   resolve: {
